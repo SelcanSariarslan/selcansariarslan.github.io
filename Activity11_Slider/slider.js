53f0800bded02b3a7fd666e6ba0eab22d1822f99
SelcanSariarslan <97344497+SelcanSariarslan@users.noreply.github.com>
2022-05-29 22:41:04 +0300

    Update slider.js

--- Activity11_Slider/slider.js
+++ Activity11_Slider/slider.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
         	$("#slider").bxSlider({
-			  auto: true,
-			  minSlides: 2,
-			  maxSlides: 2,
-			  slideWidth: 450,
-			  slideMargin: 10,
-			  randomStart:true,
-			  moveSlides: 1,
-			  mode: 'fade',
-      auto: ($(".bxslider li").length > 1) ? true : false,
-      pager: ($(".bxslider li").length > 1) ? true : false,
-       
-       pause: 3000
+			  $("#slider").bxSlider({
+		auto: true,
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 250,
+		slideMargin: 10,
+		autoDelay: 3000,
+		randomStart: true,
+		captions: true,
+		pagerType: "short"
 	
 			});
-			/*if(slider.settings.randomStart) slider.settings.startSlide = Math.floor(Math.random() * slider.children.length);*/
+			
     	});
